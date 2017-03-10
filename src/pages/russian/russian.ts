25be43bb046a2592f5fea725b21855b5b2252bfa
Jeffrey Yankey <jeffrey.yankey@egov.com>
2017-03-10 16:34:48 -0500

    Updated key value

--- src/pages/russian/russian.ts
+++ src/pages/russian/russian.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
+import {AngularFire, FirebaseListObservable} from 'angularfire2';
+
 @Component({
 	selector: 'page-russian',
 	templateUrl: 'russian.html'
 })
+
 export class RussianPage {
 
-	public words: Array<string>;
+	words: FirebaseListObservable<any>;
 
-	constructor(public navCtrl: NavController, public navParams: NavParams) {
-
+	constructor(public navCtrl: NavController, public navParams: NavParams, af: AngularFire) {
+		this.words = af.database.list('/words');
 	}
 }
